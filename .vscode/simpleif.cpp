257004840691e22f6ed9b8a59697d5bbdb29928a
mahardikaputra12 <mahardikaqohar@gmail.com>
2026-03-13 23:59:40 +0700

    commit 1

---
+++ .vscode/simpleif.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() % 10;
+
+    if (x == 5){
+        x = x + 1;
+        cout << "nilai x sudah dikoreksi menjadi : " << x << endl;
+    }
+
+    cout << "Bilangan yang diberikan = " << x;
+    
+    return 0;
+}
+//commit 1
